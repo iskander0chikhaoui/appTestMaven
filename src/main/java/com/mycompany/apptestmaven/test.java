320254995f50c175632dfa985a262a7bebd93265
iskander0chikhaoui <iskander.chikhaoui@esprit.tn>
2020-06-04 16:07:32 +0100

    modify

--- src/main/java/com/mycompany/apptestmaven/test.java
+++ src/main/java/com/mycompany/apptestmaven/test.java
@@ -11,7 +11,7 @@ package com.mycompany.apptestmaven;
  */
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hhello Espritt");
+        System.out.println("Hhelloooo Espritt");
     }
 
     
